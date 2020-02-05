f2677f6bdc0e54ccd4f080c0685e90754b250130
Jordan Wallace-Addai <jordanwallace1223@gmail.com>
2020-02-05 15:52:20 -0500

    first commit!

---
+++ src/components/Userpage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Userpage extends Component() {
+  render() {
+    return (
+      <div>
+        <h1>Userpage</h1>
+      </div>
+    );
+  }
+}
+
+export default Userpage;
